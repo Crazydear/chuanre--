7c2c59d8fe2e8b2db99e7f577aef36c424987990
Crazydear <47840575+Crazydear@users.noreply.github.com>
2019-10-26 13:39:05 +0800

    Add files via upload

--- solve.py
+++ solve.py
@@ -6,12 +6,8 @@
 # 程序功能：用于求解不同条件下肋片的散热效率
 # 求解方法：有限容积法
 # 网格划分：同位网格
-# 模块划分：该程序包括输入模块-__init__,初始化模块-Init_temper
-#                   节点方程模块-center、boundary、corner
-#                   效率求解模块-eta、eta-1D,迭代求解模块-solve
-#                   输出模块-outprint，画图模块-pic
 # 第三方库:numpy(必需) chuanre(必需,项目已带)
-#          pandas(保存数据) matplotlib(画图)
+#          	 pandas(保存数据) matplotlib(画图)
 # ****************************************************************
 from chuanre import *
 
