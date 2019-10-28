9d3682a996619146eb94ebde30a891b7f6cb85f2
Crazydear <47840575+Crazydear@users.noreply.github.com>
2019-10-28 19:09:29 +0800

    Update solve.py

--- solve.py
+++ solve.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     N = 4     # 定义网格行数量
     heat = chre(0.06, 0.02)         # 肋片高度和半个厚度
     # 定义肋根温度和初场温度
-    heat.Init_temper(M, N, gen=100, other=60)
+    heat.Init_temper(M, N, other=60)
     heat.solve(100)         # 迭代次数，自定义
     heat.outprint()         # 数据保存和输出
     heat.pic()              # 画图
